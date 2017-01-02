d485184b6c72e4ce0cdf76fa6d878a8993c82334
andrew.stephens <andrew.stephens@kaseya.com>
2017-01-02 09:25:24 -0500

    Ignored some files

--- gensite/gensite.py
+++ gensite/gensite.py
@@ -24,20 +24,20 @@ class CommandError(Exception):
 def init():
   if len(os.listdir(path='.')) != 0:
     raise CommandError("Current directory not empty")
-    
+
   blog_dir = os.getcwd()
   user_config = os.path.join(os.path.expanduser("~"), ".gensite")
-  
+
   if (os.path.exists(user_config)):
     raise CommandError("User already has a site in the " + user_config + " file, delete first")
-  
+
   user_site_data = { "source_dir" : blog_dir,
                      "deploy_command" : "run deploy script here" }
-                     
+
   with open(user_config, "w", encoding="utf-8") as f:
     json.dump(user_site_data, f)
-  
-    
+
+
   site_metadata = {
     "source_dir" : ".",
     "destination_dir" : "/Users/andrew/Source/gensite/webbuild",
@@ -49,23 +49,23 @@ def init():
     "relative_index" : "/",
     "allowed_tags" : []
   }
-  
+
   with open("config.js", "w", encoding="utf-8") as f:
     json.dump(site_metadata, f)
-  
+
   print("OK - edit config.js file")
-  
+
 def read_user_config():
   user_config_file = os.path.join(os.path.expanduser("~"), ".gensite")
-  
+
   if not os.path.exists(user_config_file):
     raise CommandError("No user file exists, use gensite init first : " + user_config_file)
-  
+
   user_config = {}
   with open(user_config_file, "r", encoding="utf-8") as f:
     user_config = json.load(f)
   return user_config
-    
+
 def new():
   """ creates a file from the template """
   user_config = read_user_config()
@@ -73,25 +73,25 @@ def new():
   site_config = siteconfig.SiteConfig(base_dir)
   author = site_config.blog_author
   title = input("Enter the title: ")
-  
-  p = files.create_new_article(base_dir, title, author, time.gmtime()) 
+
+  p = files.create_new_article(base_dir, title, author, time.gmtime())
 
   print(p, "created")
-  
+
 def build():
   user_config = read_user_config()
   base_dir = user_config["source_dir"]
   site_config = siteconfig.SiteConfig(base_dir)
-  
+
   files.gensite(base_dir)
-  
+
 def deploy():
   user_config = read_user_config()
   base_dir = user_config["source_dir"]
   site_config = siteconfig.SiteConfig(base_dir)
   build_dir = site_config.destination_dir
   command = user_config["deploy_command"]
-  
+
   subs_command = []
   for a in command:
     if a == "{{build_dir}}":
@@ -101,30 +101,29 @@ def deploy():
       subs_command.append(t)
     else:
       subs_command.append(a)
-    
+
   print("Running: " + str(subs_command))
-  
+
   subprocess.run(subs_command)
-  
+
 def print_valid_tags():
   user_config = read_user_config()
   base_dir = user_config["source_dir"]
   site_config = siteconfig.SiteConfig(base_dir)
-  
+
   print("Valid tags:")
   for t in site_config.allowed_tags:
     print("   ", t)
 
-  
+
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("command", choices=["init", "build", "deploy", "clean", "new", "list", "tags"])
   args = parser.parse_args()
-  
+
   {'init' : init ,
    'new' : new,
     'build' : build,
     'deploy' : deploy,
     'tags' : print_valid_tags}[args.command]()
-   
